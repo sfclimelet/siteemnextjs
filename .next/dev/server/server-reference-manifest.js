d9f5d91c1ac9fb8273335190793a729ccf3d9a16
edvandro <dudussx777@gmail.com>
2025-11-21 22:00:02 -0400

    Navbar Melhoradagit statusgit status!

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7LaK0hER4bUkMIj8KrnrFG8oayf1xyazQKbhcjK28Os=\"\n}"
--- .next/dev/static/chunks/app_page_jsx_b3d2b5d8._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_49da1a74._.js",
-  "static/chunks/node_modules_66a1a922._.js",
-  "static/chunks/styles_acc2a091._.css"
-],
-    source: "dynamic"
-});
