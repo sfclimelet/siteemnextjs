4fc5d7f1434e455bac650246d7e015def306dbec
edvandro <dudussx777@gmail.com>
2025-12-05 19:08:42 -0400

    Bugs nas resoluções resolvidos.

--- components/pages/SectionLojaSEF.jsx
+++ components/pages/SectionLojaSEF.jsx
@@ -18,17 +18,17 @@ export default function SectionLojaSEF() {
                     <div className="card-curve"></div>
                 </div>
 
-                {/* CONTEÚDO DO CARD */}
-                <h2 className="loja-title">Conheça nossa Loja</h2>
-
-                <p className="loja-descricao">
-                    Ar-condicionado de última geração para sua casa ou empresa.
-                    Eficiência, design moderno e instalação profissional garantida.
-                </p>
-
-                <Link href="#" className="btn-loja" aria-label="Comprar Agora" title="Comprar Agora">
-                    Comprar Agora <ShoppingBag />
-                </Link>
+                <div className="card-bd">
+                    {/* CONTEÚDO DO CARD */}
+                    <h2 className="loja-title">Conheça nossa Loja</h2>
+                    <p className="loja-descricao">
+                        Ar-condicionado de última geração para sua casa ou empresa.
+                        Eficiência, design moderno e instalação profissional garantida.
+                    </p>
+                    <Link href="#" className="btn-loja" aria-label="Comprar Agora" title="Comprar Agora">
+                        Comprar Agora <ShoppingBag />
+                    </Link>
+                </div>
             </div>
         </>
     );
