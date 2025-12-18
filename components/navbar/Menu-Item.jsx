e313382275e8abd51ccc61a820b2f5a93d0a90cd
edvandro <dudussx777@gmail.com>
2025-12-18 17:27:09 -0400

    Adicionando barra vertical para separar search de outros icones feito...

--- components/navbar/Menu-Item.jsx
+++ components/navbar/Menu-Item.jsx
@@ -5,20 +5,24 @@ export default function MenuItem({ item }) {
   // SEARCH é um tipo especial
   if (item.type === "search") {
     return (
-      <li id={`LI-${item.id}`} className="nav-item dropdown">
-        <button id={item.buttonId} className="nav-link nav-icon" data-bs-toggle="dropdown">
-          {item.icon}
-        </button>
+      <>
+        <li id={`LI-${item.id}`} className="nav-item dropdown">
+          <button id={item.buttonId} className="nav-link nav-icon" data-bs-toggle="dropdown">
+            {item.icon}
+          </button>
 
-        <ul className="dropdown-menu dropdown-menu-end search-dropdown">
-          <li>
-            <div className="dropdown-search-box">
-              <input className="form-control search-input" placeholder="Buscar..." />
-              <button className="search-btn">{item.icon}</button>
-            </div>
-          </li>
-        </ul>
-      </li>
+          <ul className="dropdown-menu dropdown-menu-end search-dropdown">
+            <li>
+              <div className="dropdown-search-box">
+                <input className="form-control search-input" placeholder="Buscar..." />
+                <button className="search-btn">{item.icon}</button>
+              </div>
+            </li>
+          </ul>
+        </li>
+        {/* Barra Vertical */}
+        <div className="nb-divider" aria-hidden="true" ></div>
+      </>
     );
   }
 
