7cb19ab0b22b0a0710c21d67ca1fe7b92ab177f8
edvandro <dudussx777@gmail.com>
2026-01-08 16:38:10 -0400

    Layout base para mobile 375px feito...

--- components/footer/Footer-Home.jsx
+++ components/footer/Footer-Home.jsx
@@ -7,51 +7,47 @@ export default function FooterHome() {
   return (
     <footer id="footer-home">
       <div className="footer-container">
+            {/* LOGO */}
+            <div className="footer-brand">
+                <img
+                    src={FooterHomeData.logo.src.src}
+                    alt={FooterHomeData.logo.alt}
+                />
+                <p>{FooterHomeData.slogan}</p>
+            </div>
 
-        {/* LOGO */}
-        <div className="footer-brand">
-          <img
-            src={FooterHomeData.logo.src.src}
-            alt={FooterHomeData.logo.alt}
-          />
-          <p>{FooterHomeData.slogan}</p>
-        </div>
+            {/* LINKS */}
+            <nav className="footer-links">
+                <h3>Links Rápidos</h3>
+                <ul>
+                    {FooterHomeData.links.map((link) => (
+                    <li key={link.label}>
+                        <a href={link.href}>{link.label}</a>
+                    </li>
+                    ))}
+                </ul>
+            </nav>
 
-        {/* LINKS */}
-        <nav className="footer-links">
-          <h3>Links Rápidos</h3>
-          <ul>
-            {FooterHomeData.links.map((link) => (
-              <li key={link.label}>
-                <a href={link.href}>{link.label}</a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-
-        {/* SOCIAIS */}
-        <div className="footer-socials">
-          <h3>Redes Sociais</h3>
-
-          <div className="sociais">
-            {FooterHomeData.socials.map((item) => {
-              const Icon = item.icon;
-
-              return (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label={item.name}
-                >
-                  <Icon />
-                </a>
-              );
-            })}
-          </div>
-        </div>
-
+            {/* SOCIAIS */}
+            <div className="footer-socials">
+                <h3>Redes Sociais</h3>
+                <div className="sociais">
+                    {FooterHomeData.socials.map((item) => {
+                    const Icon = item.icon;
+                    return (
+                        <a
+                        key={item.name}
+                        href={item.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={item.name}
+                        >
+                        <Icon />
+                        </a>
+                    );
+                    })}
+                </div>
+            </div>
       </div>
 
       <div className="footer-divider"></div>
